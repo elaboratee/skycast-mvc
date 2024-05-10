48b37bbd318daf7bc45cc233d3dc0b3ac689b34d
Ilya Sokol <innocentbeing911@gmail.com>
2024-05-10 23:16:15 +0300

    Добавление зависимостей и стартовых параметров конфигурации

---
+++ src/main/java/sc/skycastmvc/SkycastMvcApplication.java
@@ -0,0 +1,13 @@
+package sc.skycastmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkycastMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkycastMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sc/skycastmvc/SkycastMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package sc.skycastmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkycastMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
