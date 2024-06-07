090460efaeecea95bda8bb9df2dbde88c788eb92
Kirill Lapin <thenedafreid@gmail.com>
2024-06-07 17:08:18 +0300

    SC-44: Изменены названия страниц, добавлена кнопка личного кабинета

---
+++ src/main/java/sc/skycastmvc/controller/AccountController.java
@@ -0,0 +1,14 @@
+package sc.skycastmvc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+public class AccountController {
+
+}
